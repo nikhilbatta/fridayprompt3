f9898b102176800d9bdca245373f30f681df19c7
nikhil batta <nbatta08@gmail.com>
2019-08-23 10:24:03 -0700

    figured out how to replace with beep bop, now will try to implent a forloop so if 113 contains 13 it will give you the same output

--- Desktop/fridayprompt3/js/scripts.js
+++ Desktop/fridayprompt3/js/scripts.js
@@ -28,3 +28,25 @@ if(index>-1){
  return arr.splice(index,1)
 }
 }
+// try three
+var number = 113;
+var arr = []
+
+for (var i = 0; i<number; i++){
+  arr.push(i)
+
+}
+var slicer = arr.toString();
+lookForNumbers()
+
+function lookForNumbers(){
+ slicer = slicer.replace(1,"beep")
+ slicer = slicer.replace(2,"boop")
+ slicer = slicer.replace(3,"Im sorry dave, I cant do that")
+ slicer = slicer.replace(13,"Im sorry dave,I cant do that")
+ slicer = slicer.replace(21,"Boop")
+ slicer = slicer.replace(32, "Im sory dave i cant do that")
+}
+
+
+ console.log(slicer)
