9598439cbe5b4e408f95ad7524d55febc7c687ed
nikhil batta <nbatta08@gmail.com>
2019-08-23 14:37:33 -0700

    order of precedence to find the new value of the 123

--- Desktop/fridayprompt3/js/scripts.js
+++ Desktop/fridayprompt3/js/scripts.js
@@ -2,27 +2,41 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
   event.preventDefault();
   var number = parseInt($("#grabNumber").val());
-  var startOfLogic = firstFunction(number)
-  $("#formOne").append("<li>" + lookForNumbers() + "</li>")
+  $("#formOne").append("<li>" + firstFunction(number)  + "</li>")
 });
 });
 // try three
-var arr = []
+
 function firstFunction(userNumber){
-for (var i = 0; i<userNumber; i++){
+var arr = []
+for (var i = 0; i<=userNumber; i++){
   arr.push(i)
 }
 console.log(arr)
+
+return lookForNumbers(arr);
+}
+function lookForNumbers(arr){
+  // var slicer = arr.toString();
+for (var j=0;j<arr.length;j++){
+    var numString = arr[j].toString()
+    if(numString.includes("3")){
+      arr[j] = "Im sorry dave, I cant do that"
+    }
+    else if (numString.includes("2")) {
+      arr[j]="boop";
+    }
+    else if(numString.includes("1")){
+    arr[j]= "beep";
+  }
+}
 return arr;
-lookForNumbers();
 }
-function lookForNumbers(){
-  var slicer = arr.toString();
- slicer = slicer.replace(1,"beep")
- slicer = slicer.replace(2,"boop")
- slicer = slicer.replace(3,"Im sorry dave, I cant do that")
- slicer = slicer.replace(13,"Im sorry dave,I cant do that")
- slicer = slicer.replace(21,"Boop")
- slicer = slicer.replace(32, "Im sory dave i cant do that")
- return slicer;
-}
+ //
+ //
+ //  slicer = slicer.replace(1,"beep")
+ // slicer = slicer.replace(2,"boop")
+ // slicer = slicer.replace(3,"Im sorry dave, I cant do that")
+ // slicer = slicer.replace(13,"Im sorry dave,I cant do that")
+ // slicer = slicer.replace(21,"Boop")
+ // slicer = slicer.replace(32, "Im sory dave i cant do that")
