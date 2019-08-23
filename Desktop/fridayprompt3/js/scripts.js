8a2840eb768b87ca3cb8a6e06e38f15eb8c75613
nikhil batta <nbatta08@gmail.com>
2019-08-23 13:18:41 -0700

    got ui logic and business logic to mash

--- Desktop/fridayprompt3/js/scripts.js
+++ Desktop/fridayprompt3/js/scripts.js
@@ -1,27 +1,28 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event){
   event.preventDefault();
-  $("formOne").append("<li>" + slicer + "</li>")
+  var number = parseInt($("#grabNumber").val());
+  var startOfLogic = firstFunction(number)
+  $("#formOne").append("<li>" + lookForNumbers() + "</li>")
 });
 });
 // try three
 var arr = []
-var number = parseInt($("#grabNumber").val());
-for (var i = 0; i<number; i++){
+function firstFunction(userNumber){
+for (var i = 0; i<userNumber; i++){
   arr.push(i)
-  console.log(arr)
 }
-var slicer = arr.toString();
+console.log(arr)
+return arr;
 lookForNumbers();
+}
 function lookForNumbers(){
-
+  var slicer = arr.toString();
  slicer = slicer.replace(1,"beep")
  slicer = slicer.replace(2,"boop")
  slicer = slicer.replace(3,"Im sorry dave, I cant do that")
  slicer = slicer.replace(13,"Im sorry dave,I cant do that")
  slicer = slicer.replace(21,"Boop")
  slicer = slicer.replace(32, "Im sory dave i cant do that")
+ return slicer;
 }
-
-
- console.log(slicer)
