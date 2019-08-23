ec1b90613de26ec05f80db079a4c8d9f27c2f671
nikhil batta <nbatta08@gmail.com>
2019-08-23 12:20:29 -0700

    mixing business and ui logic

--- Desktop/fridayprompt3/js/scripts.js
+++ Desktop/fridayprompt3/js/scripts.js
@@ -1,45 +1,20 @@
-var number = 1;
-var arr = []
-for (var i = number; i > 0; i--){
-  arr.push(i)
-}
- console.log(arr)
-arr.forEach(function(num){
-  if (num == 1){
-
-    arr[num]= "beep"
-    arr.push(arr[num])
-  }
-})
-
-
-console.log(arr)
- // try two
-var number = 19;
-var arr = []
-for (var i = 0; i<number; i++){
-  arr.push(i)
-
-}
-lookForNumbers()
-function lookForNumbers(){
- var index = arr.indexOf(1)
-if(index>-1){
- return arr.splice(index,1)
-}
-}
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+  event.preventDefault();
+  var number = parseInt($("#grabNumber").val());
+  $("formOne").append("<li>" + slicer + "</li>")
+});
+});
 // try three
-var number = 113;
 var arr = []
-
+var number = ""
 for (var i = 0; i<number; i++){
   arr.push(i)
-
 }
 var slicer = arr.toString();
-lookForNumbers()
 
 function lookForNumbers(){
+
  slicer = slicer.replace(1,"beep")
  slicer = slicer.replace(2,"boop")
  slicer = slicer.replace(3,"Im sorry dave, I cant do that")
@@ -47,6 +22,6 @@ function lookForNumbers(){
  slicer = slicer.replace(21,"Boop")
  slicer = slicer.replace(32, "Im sory dave i cant do that")
 }
-
-
+return slicer;
+lookForNumbers()
  console.log(slicer)
